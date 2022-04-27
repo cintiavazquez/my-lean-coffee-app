9985f763a61165f447d85bb8583586d2a9961073
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-04-27 14:54:19 +0200

    add apis and create form

---
+++ pages/api/card/[id].js
@@ -0,0 +1,21 @@
+import { getCards } from "../../../src/services/get-cards";
+
+export default function handler(req, res) {
+  const { id } = req.query;
+  const cards = getCards();
+
+  const singleCard = cards.find((card) => card.id === id);
+
+  if (req.method === "DELETE") {
+    res.status(200).json({ message: "card deleted", card: singleCard });
+  } else if (req.method === "PUT") {
+    const changedCard = JSON.parse(req.body);
+    res.status(200).json({ message: "card updated", card: changedCard });
+  } else {
+    res.status(200).json(singleCard);
+  }
+
+  //const index = cards.findIndex((card) => card.id === id);
+
+  //res.status(200).json(cards[index]);
+}
